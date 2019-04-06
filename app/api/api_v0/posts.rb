037af8ce1b0bcaa4dad1070f8fc694097976f415
niclin <bboyceo@hotmail.com>
2019-04-06 18:53:45 +0800

    Implement API Post#create

--- app/api/api_v0/posts.rb
+++ app/api/api_v0/posts.rb
@@ -8,5 +8,20 @@ module ApiV0
 
       present posts, with: ApiV0::Entities::Post
     end
+
+    desc "Create new post"
+    params do
+      requires :title, type: String
+      requires :context, type: String
+    end
+    post "/posts" do
+      post = current_user.posts.new(declared(params, include_missing: false).except(:access_key))
+
+      if post.save
+        present post, with: ApiV0::Entities::Post
+      else
+        raise StandardError, $!
+      end
+    end
   end
 end
